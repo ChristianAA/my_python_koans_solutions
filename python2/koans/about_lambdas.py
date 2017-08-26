542f4395fad824bfdb654c826c1c5c4d35de5ca0
christianAA <christian0506@gmail.com>
2017-08-26 09:48:50 +0200

    Python2. Completed: 18. Lambdas

--- python2/koans/about_lambdas.py
+++ python2/koans/about_lambdas.py
@@ -3,6 +3,8 @@
 
 #
 # Based slightly on the lambdas section of AboutBlocks in the Ruby Koans
+# https://docs.python.org/2.7/tutorial/controlflow.html#lambda-expressions
+# https://stackoverflow.com/questions/890128/why-are-python-lambdas-useful
 #
 
 from runner.koan import *
@@ -11,7 +13,7 @@ from runner.koan import *
 class AboutLambdas(Koan):
     def test_lambdas_can_be_assigned_to_variables_and_called_explicitly(self):
         add_one = lambda n: n + 1
-        self.assertEqual(__, add_one(10))
+        self.assertEqual(11, add_one(10))
 
     # ------------------------------------------------------------------
 
@@ -22,8 +24,8 @@ class AboutLambdas(Koan):
         sausages = self.make_order('sausage')
         eggs = self.make_order('egg')
 
-        self.assertEqual(__, sausages(3))
-        self.assertEqual(__, eggs(2))
+        self.assertEqual('3 sausages', sausages(3))
+        self.assertEqual('2 eggs', eggs(2))
 
     def test_accessing_lambda_without_assignment(self):
-        self.assertEqual(__, self.make_order('spam')(39823))
+        self.assertEqual('39823 spams', self.make_order('spam')(39823))
